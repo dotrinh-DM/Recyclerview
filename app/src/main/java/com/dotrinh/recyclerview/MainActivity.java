81361c66b50fa6bbb6947562020a75297553ab34
dotrinh <thanhdo.trinh@gmail.com>
2019-06-02 14:02:05 +0900

    bai 3 - add, select, del

--- app/src/main/java/com/dotrinh/recyclerview/MainActivity.java
+++ app/src/main/java/com/dotrinh/recyclerview/MainActivity.java
@@ -28,9 +28,13 @@ import java.util.ArrayList;
 
 import static com.dotrinh.recyclerview.misc.LogUtil.LogI;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     ArrayList<Person> people = new ArrayList<>();
+    RecyclerView myRecycleView;
+    private LinearLayoutManager layoutManager;
+    ChildAdapter adapter;
+    int lastVisibleItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,18 +42,94 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RecyclerView myRecycleView = findViewById(R.id.myRecycleView);
-        myRecycleView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-//        myRecycleView.setLayoutManager(new GridLayoutManager(this, 2));
         initData();
-        myRecycleView.setAdapter(new ChildAdapter());
+        myRecycleView = findViewById(R.id.myRecycleView);
+        layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        myRecycleView.setLayoutManager(layoutManager);
+        adapter = new ChildAdapter();
+        myRecycleView.setAdapter(adapter);
+        findViewById(R.id.delActionButton).setOnClickListener(this);
+        findViewById(R.id.allActionButton).setOnClickListener(this);
+        findViewById(R.id.addActionButton).setOnClickListener(this);
+        myRecycleView.addOnScrollListener(onScrollListener);
     }
 
     void initData() {
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 10; i++) {
             people.add(new Person(i, "Dotrinh", false));
         }
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.delActionButton:
+                int c = people.size() - 1;
+                for (int i = c; i >= 0; i--) {
+                    if (people.get(i).isCheck()) {
+                        people.remove(i);
+                        adapter.notifyItemRemoved(i);
+                        c--;
+                    }
+                }
+                break;
+            case R.id.allActionButton:
+                boolean isUncheck = false;
+                for (Person it : people) {
+                    if (it.isCheck()) {
+                        isUncheck = true;
+                    } else {
+                        isUncheck = false;
+                        break;
+                    }
+                }
+                if (isUncheck) {
+                    for (Person it2 : people) {
+                        it2.setCheck(false);
+                    }
+                } else {
+                    for (Person it2 : people) {
+                        it2.setCheck(true);
+                    }
+                }
+                adapter.notifyDataSetChanged();
+                break;
+            case R.id.addActionButton:
+                int nextId;
+                if (people.size() == 0) {
+                    nextId = 0;
+                } else {
+                    nextId = (people.get(people.size() - 1).getId()) + 1;
+                }
+                people.add(new Person(nextId, "Dotrinh", false));
+                if (lastVisibleItem == nextId) {
+                    myRecycleView.scrollToPosition(nextId);
+                } else {
+                    myRecycleView.smoothScrollToPosition(nextId);
+                }
+                adapter.notifyDataSetChanged();
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    private RecyclerView.OnScrollListener onScrollListener = new RecyclerView.OnScrollListener() {
+        @Override
+        public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+            super.onScrollStateChanged(recyclerView, newState);
+        }
+
+        @Override
+        public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+            super.onScrolled(recyclerView, dx, dy);
+            int sumVisibleItem = layoutManager.getChildCount(); // tuy theo do cao cua row va do cao cua man hinh
+            int sum = layoutManager.getItemCount(); // bang voi size cua array list
+            int firstVisibleItemPos = layoutManager.findFirstVisibleItemPosition(); // dong dau tien dc hien thi tren man hinh
+            lastVisibleItem = firstVisibleItemPos + sumVisibleItem;
+        }
+    };
 
     public class ChildAdapter extends RecyclerView.Adapter<ChildAdapter.ChildViewHolder> {
 
@@ -65,7 +145,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(@NonNull ChildAdapter.ChildViewHolder holder, int position) { // this method will call every time when we refresh recyclerview
-            holder.rowItem.setText(people.get(position).getName() + " " + position);
+            holder.rowItem.setText(people.get(position).getName() + " " + people.get(position).getId());
             holder.checkBox.setChecked(people.get(position).isCheck());
             LogI("dong thu: " + position);
         }
